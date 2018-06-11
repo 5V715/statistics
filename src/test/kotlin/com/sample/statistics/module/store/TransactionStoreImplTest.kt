95888b86334a7e6777cc3e7d14d5e0be1c0c842e
Silas Schwarz <silas.schwarz@aoe.com>
2018-06-11 13:48:28 +0200

    un-smell

--- src/test/kotlin/com/sample/statistics/module/store/TransactionStoreImplTest.kt
+++ src/test/kotlin/com/sample/statistics/module/store/TransactionStoreImplTest.kt
@@ -1,14 +1,39 @@
 package com.sample.statistics.module.store
 
+import com.google.common.truth.Truth.assertThat
+import com.sample.statistics.module.validate.TransactionValidatorImpl
 import org.junit.Test
+import java.time.Instant
 
 class TransactionStoreImplTest {
 
     @Test
-    fun getAll() {
+    fun `check if the time framing works`() {
+        val validator = TransactionValidatorImpl()
+        val transactionStore = TransactionStoreImpl(validator)
+
+        transactionStore.put(Transaction(4.4,Instant.now().epochSecond))
+        transactionStore.put(Transaction(4.3,Instant.now().epochSecond - 60))
+        transactionStore.put(Transaction(5.4,Instant.now().epochSecond))
+
+        val result = transactionStore.getAll()
+        assertThat(result).hasSize(2)
     }
 
     @Test
-    fun put() {
+    fun `check that add elements validation`() {
+        val validator = TransactionValidatorImpl()
+        val transactionStore = TransactionStoreImpl(validator)
+
+        transactionStore.put(Transaction(4.4,Instant.now().epochSecond))
+        transactionStore.put(Transaction(4.3,Instant.now().epochSecond - 30))
+
+        val result = transactionStore.getAll()
+        assertThat(result).hasSize(2)
+        Thread.sleep(30000)
+        val laterResult = transactionStore.getAll()
+        assertThat(laterResult).hasSize(1)
     }
+
+
 }
